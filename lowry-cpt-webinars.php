16cb90d971ff61044fa889cb0aad690ce5fbb215
Eric Defore <eric.defore@gmail.com>
2019-03-28 14:42:28 -0400

    GH header

--- lowry-cpt-webinars.php
+++ lowry-cpt-webinars.php
@@ -9,7 +9,7 @@
  * Author URI: http://realbigmarketing.com/
  * Contributors: d4mation
  * GitHub Plugin URI: lowrysolutions/cpt-webinars
- * GitHub Branch: master
+ * GitHub Branch: develop
  */
 
 // Exit if accessed directly
